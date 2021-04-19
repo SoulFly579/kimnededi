c277cf0a723e24762a60b41f66a80c4d006f7cd9
SoulFly579 <soulfly579@gmail.com>
2021-04-20 02:54:33 +0300

    Last Checking Migration

---
+++ database/migrations/2021_04_18_200630_create_page_tables.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePageTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_tables', function (Blueprint $table) {
+            $table->id();
+            $table->integer("order");
+            $table->string("title");
+            $table->text("content");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('page_tables');
+    }
+}
